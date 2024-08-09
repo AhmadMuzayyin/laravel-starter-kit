8933ce3f1cdb5a001c87b29c054ce7421beaccb5
AhmadMuzayyin <muzayyin654@gmail.com>
2024-08-10 00:19:03 +0700

    Implement webadmin template

--- resources/views/avatar.blade.php
+++ resources/views/avatar.blade.php
@@ -6,10 +6,28 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>avatar</title>
+
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 
 <body>
-    <img src="{{ $avatar }}" alt="">
+    <img src="{{ $avatar }}" class="img" alt="">
+
+
+    <script>
+        document.addEventListener('DOMContentLoaded', () => {
+            // Pastikan jQuery sudah tersedia
+            if (window.$) {
+                console.log('jQuery is loaded');
+                window.$('.img').on('click', function() {
+                    alert('Hello, World!');
+                });
+            } else {
+                console.log('jQuery is not loaded');
+            }
+        });
+    </script>
+
 </body>
 
 </html>
--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,14 @@
 <?php
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     $avatar = App\Models\User::find(1)->avatar;
     return view('avatar', compact('avatar'));
 });
+
+Auth::routes();
+
+Route::get('/', [App\Http\Controllers\Home\HomeController::class, 'root']);
+Route::get('{any}', [App\Http\Controllers\Home\HomeController::class, 'index'])->name('index');
